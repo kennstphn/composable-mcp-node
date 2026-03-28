ad351037bf8be542d88b68ef34a3ec51c849b6fb
Ken Sherman <ken.s.sherman@gmail.com>
2026-03-28 06:50:21 -0400

    Update App initialization with environment variables

--- main.mjs
+++ main.mjs
@@ -1,3 +1,8 @@
 import {App} from 'src/app.mjs';
 
-(new App({}).listen();
+(new App({
+  PORT:8787
+  DIRECTUS_BASE_URL=process.env.DIRECTUS_BASE_URL,
+  NODE_ENV:process.env.NODE_ENV,
+  DIRECTUS_TOKEN:process.env.DIRECTUS_TOKEN
+}).listen();
